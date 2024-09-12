1276fc32cd4738e9ccc95caac2694b2b9160b491
shabbir811 <uandi.ss8.ss@gmail.com>
2024-09-12 15:58:45 +0500

    add

---
+++ milestone-1-2/script.ts
@@ -0,0 +1,18 @@
+const toggleBtn = document.getElementById("toggle-btn")as HTMLButtonElement
+const skillSec  =document.getElementById("skill-sec") as HTMLElement
+
+toggleBtn.addEventListener("click", ()=>{
+
+    if(skillSec.style.display !== "none"){
+        skillSec.style.display="none";
+        toggleBtn.style.backgroundColor ="#de2f2f"
+        toggleBtn.innerHTML = "Show"
+        
+        
+    }else{
+        skillSec.style.display="block";
+        toggleBtn.style.backgroundColor ="#007bff"
+        toggleBtn.innerHTML = "Hide"
+    
+    }
+});

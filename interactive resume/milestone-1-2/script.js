24e733060493c8b514340fdfe54b5541b4676b33
Ghulam shabbir <159156027+Shabbir811@users.noreply.github.com>
2024-09-12 16:07:57 +0500

    Update script.js

--- interactive resume/milestone-1-2/script.js
+++ interactive resume/milestone-1-2/script.js
@@ -4,11 +4,11 @@ toggleBtn.addEventListener("click", function () {
     if (skillSec.style.display !== "none") {
         skillSec.style.display = "none";
         toggleBtn.style.backgroundColor = "#de2f2f";
-        toggleBtn.innerHTML = "Show";
+        toggleBtn.innerHTML = "Show skills";
     }
     else {
         skillSec.style.display = "block";
         toggleBtn.style.backgroundColor = "#007bff";
-        toggleBtn.innerHTML = "Hide";
+        toggleBtn.innerHTML = "Hide skills";
     }
 });
